e27fce8f5cb01003cd8dad0f4cb64ce3b48dd8ef
Prash <ackphd@gmail.com>
2010-03-31 02:38:52 -0400

    Updated EVERYTHING.
    Using thedude's new cmtheme overlay. Mau5 goodies added including menu background and icons.  Enjoy.

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,6 +1,4 @@
 #
-# Copyright (C) 2009 The Android Open Source Project
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -25,5 +23,7 @@
 # it includes.
 #
 
+# Change 'default' to match your theme name
+
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/prashmau5.mk
+	$(LOCAL_DIR)/theme_default_cyanogen.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,6 +3,9 @@
 # defined in envsetup.sh.
 #
 # In particular, you can add lunch options with the add_lunch_combo
-# function: add_lunch_combo generic-eng
-
-add_lunch_combo prashmau5-eng
+# Note: only change the name portion
+# function: add_lunch_combo cyanogen_theme_default-eng
+# 
+#
+# Change 'default' to make your theme name.
+add_lunch_combo theme_default_cyanogen-eng
